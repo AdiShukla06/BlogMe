5cb89c6ed1afe3c3d0d951bc903946cf046b1be1
Aditya <aditya.vishwabharati@gmail.com>
2024-08-10 01:28:54 +0530

    content debugging

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -22,10 +22,10 @@ export class Service{
                 slug,
                 {
                     title,
+                    content,
                     featuredImage,
                     status,
                     userId,
-                    content
                 }
             )
         } catch (error) {
@@ -41,10 +41,9 @@ export class Service{
                 slug,
                 {
                     title,
-                    
+                    content,
                     featuredImage,
                     status,
-                    content
 
                 }
             )
